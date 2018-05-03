0aa49f8701be4e789b7e76c55796afd0ef910b3a
劉冠呈 <tommyliu0108@gmail.com>
2018-05-03 19:31:21 +0800

    update at May 3
    testing

---
+++ app/src/main/java/com/example/gavin/unnamed_diary/writingPage.java
@@ -0,0 +1,32 @@
+package com.example.gavin.unnamed_diary;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class writingPage extends AppCompatActivity {
+    theDataBase db;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_writing_page);
+        db = new theDataBase(this);
+        Button btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText printing = findViewById(R.id.printing);
+
+                String intput = printing.getText().toString();
+                db.insert(intput);
+
+            }
+        });
+    }
+}
